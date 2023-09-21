393ad397bf53e3b714c0f9a978836876e57d8c3d
Shubhankar <shubhankartrivedi1234@gmail.com>
2023-09-22 02:27:34 +0530

    CSS fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     animation: {
       "fade-in": "fadeIn 5s ease-in-out",
       "pop-in": "popIn 8s ease-in-out",
-    },
+    }, 
     keyframes: () => ({
       fadeIn: {
         "0%": { opacity: 0, transform: "scale(100%)" },
