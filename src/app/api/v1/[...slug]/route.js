0fda54587ed98d5440bf81f8559f649457959ae0
Shubhankar <shubhankartrivedi1234@gmail.com>
2023-09-25 14:50:37 +0530

    v1.0

---
+++ src/app/api/v1/[...slug]/route.js
@@ -0,0 +1,62 @@
+import { NextResponse } from 'next/server';
+import fs from 'fs';
+import path from 'path';
+
+export async function GET(request, { params }) {
+    const { slug } = params;
+
+    const isChapter = slug[0] === 'chapter';
+    const chapter = slug[1] || null;
+
+    const isVerse = slug[2] === 'verse';
+    const verse = slug[3] || null;
+
+    const gitaFolderPath = path.join(process.cwd(), 'src', 'gita');
+
+    if (!fs.existsSync(path.join(gitaFolderPath, 'chapters.json'))) {
+        return NextResponse.json({ error: 'Chapters data not found' }, { status: 404 });
+    }
+
+    const chaptersData = fs.readFileSync(path.join(gitaFolderPath, 'chapters.json'), 'utf-8');
+    const chaptersArray = JSON.parse(chaptersData);
+
+    if (isChapter && !chapter) {
+        return NextResponse.json({ chapters: chaptersArray }, { status: 200 });
+    }
+
+    if (isChapter && chapter && !isVerse) {
+        const foundChapter = chaptersArray.find(ch => ch.chapter_number == chapter);
+
+        if (!foundChapter) {
+            return NextResponse.json({ error: 'Chapter not found' }, { status: 404 });
+        }
+
+        return NextResponse.json({ chapter: foundChapter }, { status: 200 });
+    }
+
+    if (isChapter && chapter && isVerse) {
+        const chapterFolderPath = path.join(gitaFolderPath, chapter);
+        const versesFilePath = path.join(chapterFolderPath, 'verses.json');
+
+        if (!fs.existsSync(versesFilePath)) {
+            return NextResponse.json({ error: 'Verses not found for the specified chapter' }, { status: 404 });
+        }
+
+        const versesData = fs.readFileSync(versesFilePath, 'utf-8');
+        const versesArray = JSON.parse(versesData);
+
+        if (verse) {
+            const foundVerse = versesArray.find(v => v.verse_number == verse);
+
+            if (!foundVerse) {
+                return NextResponse.json({ error: 'Verse not found' }, { status: 404 });
+            }
+
+            return NextResponse.json({ verse: foundVerse }, { status: 200 });
+        }
+
+        return NextResponse.json({ verses: versesArray }, { status: 200 });
+    }
+
+    return NextResponse.json({ error: 'Invalid request' }, { status: 400 });
+}
